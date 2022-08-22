6b53fc43b320d0dd0a7db7b0843d37f2046a0111
kamal <kamade123@gmail.com>
2022-08-22 21:03:47 +0100

    changed route for password

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,7 +8,7 @@ router.post('/login', userController.login);
 router.post('/user/logout', auth, userController.logout_post);
 router.post('/user/logoutall',  auth, userController.logoutall_post);
 router.patch('/user/edit/:id', auth, userController.edit_user);
-router.patch('/password/:id', auth, userController.password_patch);
+router.patch('/user/edit/password/:id', auth, userController.password_patch);
 router.get('/user/profile/:id', auth, userController.get_profile);
 
 
